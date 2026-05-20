5c3415e5f8e26ef60c69bc0499f10e04496a9cc0
xNoerPlaysCodes <carishma.akhauri81@gmail.com>
2026-05-20 21:21:44 +0530

    tester: beautify output

--- tests/tester.cpp
+++ tests/tester.cpp
@@ -11,7 +11,11 @@
 #include <fcntl.h>
 extern char **environ;
 int main() {
+    std::cout << "\e[38;5;220m[[ kSTL Unit Tests ]]\e[0m\n";
+
     int dev_null = open("/dev/null", O_WRONLY);
+    int passed = 0;
+    int failed = 0;
     for (auto &entry : std::filesystem::recursive_directory_iterator("bin/tests")) {
         if (entry.is_regular_file()) {
             pid_t child;
@@ -25,7 +29,7 @@ int main() {
             auto path = entry.path();
             auto relative = path.lexically_relative("bin/tests");
 
-            std::cout << relative.string() << " ..." << std::flush;
+            std::cout << ' ' << relative.string() << '\n' << std::flush;
             std::this_thread::sleep_for(std::chrono::milliseconds(20));
 
             posix_spawn(&child, path.c_str(), &actions, nullptr, args.data(), environ);
@@ -34,12 +38,18 @@ int main() {
             waitpid(child, &status, 0);
 
             if (status != 0) {
-                std::cout << "Test " << relative.string() << " has failed with exit code: " << status << '\n';
+                std::cout << ' ' << "\e[2K\e[38;5;196m" << "Failure: " << "\e[0m" << "(exit code " << status << ")" << '\n' << std::endl;
+                ++failed;
             } else {
-                std::cout << "\r\033[2K" << relative.string() << ": " << "Success" << std::endl;
+                std::cout << ' ' << "\e[2K\e[38;5;154mSuccess\e[0m" << '\n' << std::endl;
+                ++passed;
             }
         }
     }
+
+    std::cout << "--> \e[38;5;220mResults\e[0m <--\n";
+    std::cout << "\e[38;5;196m" << "Failed: " << "\e[0m" << failed << '\n';
+    std::cout << "\e[38;5;154m" << "Passed: " << "\e[0m" << passed << '\n';
 }
 #else
 int main() {
